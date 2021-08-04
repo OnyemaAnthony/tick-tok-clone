b1c37b31e3b3783996947794e113537598f39b7a
OnyemaAnthony <Anthonychibuike246@gmail.com>
2021-08-04 02:19:15 +0000

    setup firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,2 +1,15 @@
 import firebase from 'firebase';
 
+const firebaseConfig = {
+    apiKey: "AIzaSyCGTFFsIw2AFgewMBD85sH9TahzWBEEzCE",
+    authDomain: "tick-tok-clone-d83e8.firebaseapp.com",
+    projectId: "tick-tok-clone-d83e8",
+    storageBucket: "tick-tok-clone-d83e8.appspot.com",
+    messagingSenderId: "513289900722",
+    appId: "1:513289900722:web:8dce182dd3bfbc19082fab",
+    measurementId: "G-W55RBMQZ6Z"
+};
+
+const  db = firebaseConfig.firestore();
+
+export {db};
